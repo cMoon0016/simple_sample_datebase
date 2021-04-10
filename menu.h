32b66f541ce97872d42b98a561a5be9b4c92c70c
cMoon0016 <82235157+cMoon0016@users.noreply.github.com>
2021-04-10 19:11:10 +0200

    Update menu.h

--- menu.h
+++ menu.h
@@ -4,6 +4,6 @@
 #include "probka.h"
 
 void menu_wyswietl();
-void aktualnie_wybrana_probka(vector<Probka> &probki, int indeks_wybranej_probki);
+void aktualnie_wybrana_probka(std::vector<Probka> &probki, int indeks_wybranej_probki);
 
 #endif // MENU_H_INCLUDED
