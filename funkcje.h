5d65254a12f173786e5b305275e402adb64a737a
cMoon0016 <82235157+cMoon0016@users.noreply.github.com>
2021-04-10 19:10:36 +0200

    Update funkcje.h

--- funkcje.h
+++ funkcje.h
@@ -2,15 +2,15 @@
 #define FUNKCJE_H_INCLUDED
 #include <vector>
 
-int znajdz_po_nazwie(vector<Probka> probki, int &indeks_wybranej_probki);
-void zapisz_do_pliku(vector<Probka> probki);
-string wytnij_do_odczytu(string &napis);
-string wytnij_do_daty(string &wyciete);
-void odczytaj_z_pliku(vector<Probka> &probki);
-void wyszukaj_po_niepelnej_nazwie(vector<Probka> probki, int &indeks_wybranej_probki);
-void wyswietl_liste_probek(vector<Probka> probki);
-void usun_probke(vector<Probka> &probki, int &indeks_wybranej_probki);
-bool czy_jest_liczba(string &napis);
+int znajdz_po_nazwie(std::vector<Probka> probki, int &indeks_wybranej_probki);
+void zapisz_do_pliku(std::vector<Probka> probki);
+std::string wytnij_do_odczytu(std::string &napis);
+std::string wytnij_do_daty(std::string &wyciete);
+void odczytaj_z_pliku(std::vector<Probka> &probki);
+void wyszukaj_po_niepelnej_nazwie(std::vector<Probka> probki, int &indeks_wybranej_probki);
+void wyswietl_liste_probek(std::vector<Probka> probki);
+void usun_probke(std::vector<Probka> &probki, int &indeks_wybranej_probki);
+bool czy_jest_liczba(std::string &napis);
 
 
 #endif // FUNKCJE_H_INCLUDED
